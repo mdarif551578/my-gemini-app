683f73f5951d631e5dc8a2ac4d4acb708a8fb8da
mda510624 <mda510624@gmail.com>
2024-10-16 18:04:17 +0000

    ...

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import json
-import os
-
-import google.generativeai as genai
 from flask import Flask, jsonify, request, send_file, send_from_directory
-
-# 🔥🔥 FILL THIS OUT FIRST! 🔥🔥
-# Get your Gemini API key by:
-# - Selecting "Add Gemini API" in the "Project IDX" panel in the sidebar
-# - Or by visiting https://g.co/ai/idxGetGeminiKey
-API_KEY = 'TODO'
-
-genai.configure(api_key=API_KEY)
 
 app = Flask(__name__)
 
@@ -20,33 +9,9 @@ def index():
     return send_file('web/index.html')
 
 
-@app.route("/api/generate", methods=["POST"])
-def generate_api():
-    if request.method == "POST":
-        if API_KEY == 'TODO':
-            return jsonify({ "error": '''
-                To get started, get an API key at
-                https://g.co/ai/idxGetGeminiKey and enter it in
-                main.py
-                '''.replace('\n', '') })
-        try:
-            req_body = request.get_json()
-            content = req_body.get("contents")
-            model = genai.GenerativeModel(model_name=req_body.get("model"))
-            response = model.generate_content(content, stream=True)
-            def stream():
-                for chunk in response:
-                    yield 'data: %s\n\n' % json.dumps({ "text": chunk.text })
-
-            return stream(), {'Content-Type': 'text/event-stream'}
-
-        except Exception as e:
-            return jsonify({ "error": str(e) })
-
-
-@app.route('/<path:path>')
-def serve_static(path):
-    return send_from_directory('web', path)
+@app.route('/clicked')
+def text():
+    return "<h1>Hello the button is clicked.</h1>"
 
 
 if __name__ == "__main__":
